f2c57b125c58c85c0b68761885b3140f914c8e5a
Kevin Egstorf <kevin.egstorf@frontmen.nl>
2020-11-11 10:55:15 +0100

    feat: added reducer to implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Counter, {
   CounterDecrementButton,
   CounterIncrementButton,
   CounterInput,
+  countReducer,
+  actionTypes,
 } from "./Counter";
 
 const styles = makeStyles({
   root: {
     margin: "10em auto",
-    width: "22%",
+    width: "50vw",
   },
   gridContainer: {
     margin: "0 auto",
@@ -21,17 +23,31 @@ const styles = makeStyles({
 
 function App() {
   const classes = styles();
-  // Make state visible and useable
-  // add custom reducer like no negative numbers, max value
-  // make it multiply or add 2
+  const [state, dispatch] = React.useReducer(stateReducer, 0);
+  function stateReducer(state: number, action: { type: string }) {
+    return countReducer(state, action);
+  }
+
   return (
     <Card className={classes.root} elevation={6}>
       <Grid className={classes.gridContainer} container>
         <Grid className={classes.gridItem}>
-          <Counter value={0}>
-            <CounterDecrementButton>-</CounterDecrementButton>
-            <CounterInput />
-            <CounterIncrementButton>+</CounterIncrementButton>
+          <Counter value={state}>
+            <CounterDecrementButton
+              onClick={() => {
+                dispatch({ type: actionTypes.decrement });
+              }}
+            >
+              -
+            </CounterDecrementButton>
+            <CounterInput value={state} />
+            <CounterIncrementButton
+              onClick={() => {
+                dispatch({ type: actionTypes.increment });
+              }}
+            >
+              +
+            </CounterIncrementButton>
           </Counter>
         </Grid>
       </Grid>
